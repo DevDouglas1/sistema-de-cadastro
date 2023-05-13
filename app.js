a86540ff5ef4f442cd29376c9d0244c6057694a8
DevDouglas1 <douglasecomercy@gmail.com>
2023-05-13 01:31:40 -0300

    novas informacoes de git para postagens no github

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 //git push empurra para o github;
 //git status verifica o status do commit;
 //git log --online (log por linha do github);
+//git log -p (Se, em vez de menos informações, quisermos ver mais como as alterações do commit, podemos usar);
+//git log --author="user_name" (Também podemos pesquisar as informações do autor daquele commit com o comando);
+//git log --since=1.month.ago --until=1.day.ago (E pesquisar informações por data);
+//git log --pretty="format:%h %s" (No comando acima, estamos buscando as informações do commit desde um mês atrás até um dia atrás Você também pode formatar a visualização das informações de commit com o comando);
