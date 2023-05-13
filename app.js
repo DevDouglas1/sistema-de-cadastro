3d9617260db739dd3f3289bf2ae1382f5a451518
DevDouglas1 <douglasecomercy@gmail.com>
2023-05-13 01:28:22 -0300

    informacoes dos comandos do git

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
+
+
+
+//git commit (arquivo que quer modificar/postar) -m "FRASE PARA ALTERAR NO GITHub";
+//git push empurra para o github;
+//git status verifica o status do commit;
+//git log --online (log por linha do github);
